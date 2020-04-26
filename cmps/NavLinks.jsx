cde96a5d6862fb28bd9865f80059a440966d2ff8
Your Name <daniel720p@gmail.com>
2020-04-27 00:30:22 +0300

    nav bar functional and pretty nav links

--- cmps/NavLinks.jsx
+++ cmps/NavLinks.jsx
@@ -1,20 +1,20 @@
 const { NavLink } = ReactRouterDOM;
 
-export default function NavLinks() {
+export default function NavLinks(props) {
 
     return (
         <section className="nav-links">
             <NavLink to='/'>
-                <img src="assets/imgs/logo-appsus.png" alt="appsus"/>
+                <img onClick={()=>{props.changePage('home')}} src="assets/imgs/logo-home.png" alt="appsus"/>
             </NavLink>
             <NavLink to='/email'>
-            <img src="assets/imgs/logo-email.png" alt="email"/>
+            <img onClick={()=>{props.changePage('email')}} src="assets/imgs/logo-email.png" alt="email"/>
             </NavLink>
             <NavLink to='/keep'>
-            <img src="assets/imgs/logo-keep.png" alt="keep"/>
+            <img onClick={()=>{props.changePage('keep')}} src="assets/imgs/logo-keep.png" alt="keep"/>
             </NavLink>
             <NavLink to='/book'>
-            <img src="assets/imgs/logo-books.png" alt="book"/>
+            <img onClick={()=>{props.changePage('books')}} src="assets/imgs/logo-books.png" alt="book"/>
             </NavLink>
         </section>
     )
